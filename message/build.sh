95378a0a4d1c8c6586ddb4c6fdbd2001c7a43c23
Mohammad <149521106+Mohammad-416@users.noreply.github.com>
2024-01-27 18:44:24 +0530

    Update build.sh

--- message/build.sh
+++ message/build.sh
@@ -11,4 +11,4 @@ python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('Mohammad', 'm9971359949@gmail.com', 'Mohammad@123')" | python manage.py shell
 
 # Collect static files
-python manage.py collectstatic
+python manage.py collectstatic --noinput
