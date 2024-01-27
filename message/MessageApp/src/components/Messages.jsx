f909506fa947e96392b7fb97545e38a65bad1cd5
Mohammad <m9971359949@gmail.com>
2024-01-28 00:10:07 +0530

    ws to wss

--- message/MessageApp/src/components/Messages.jsx
+++ message/MessageApp/src/components/Messages.jsx
@@ -17,7 +17,7 @@ function Messages() {
     whoami().then((username) => {
       console.log("You are logged in as: " + username);
       
-      let url = 'ws://chatsvet.onrender.com/'+ username + '/' + slugify(slug) + '/';
+      let url = 'wss://chatsvet.onrender.com/'+ username + '/' + slugify(slug) + '/';
       const newSocket = new WebSocket(url);
       setUsername(username);
       newSocket.addEventListener('open', (event) => {
