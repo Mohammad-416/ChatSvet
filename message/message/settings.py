88de9bfe46803dbf2b2621f61e19bacf6adc55cb
Mohammad <149521106+Mohammad-416@users.noreply.github.com>
2024-01-27 22:23:10 +0530

    Update settings.py

--- message/message/settings.py
+++ message/message/settings.py
@@ -112,6 +112,7 @@ DATABASES = {
 }
 database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
+CSRF_TRUSTED_ORIGINS = ['chatsvet.onrender.com']
 
 
 # Password validation
